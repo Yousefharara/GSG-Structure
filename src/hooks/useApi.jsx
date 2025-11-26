d263a20b4565c4ec6d5679e9488d203f3594e65a
Yousefharara <hrarty37@gmail.com>
2025-11-26 19:41:29 +0200

    feat: Add getById Method

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -2,20 +2,18 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const UseApi = () => {
-  // ! Error in input [ null | [] ]
+const UseApi = (url) => {
   const [data, setData] = useState([]);
+  const [item, setItem] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [message, setMessage] = useState("");
   const navigate = useNavigate();
 
-  const get = async (url, config) => {
+  const get = async (config) => {
     try {
-      console.log("From UseApi ", data, " , ", url);
       const res = await axios.get(url, { ...config });
       setIsLoading(true);
-      console.log(res.data);
       setData(res.data);
     } catch (err) {
       setError(err);
@@ -24,13 +22,25 @@ const UseApi = () => {
     }
   };
 
-  const post = async (url, body, path, config) => {
+  const getById = async (id, config) => {
+    try {
+      const res = await axios.get(`${url}/${id}`, { ...config });
+      setIsLoading(true);
+      setItem(res.data)
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const post = async (body, path, config) => {
     try {
       const res = await axios.post(url, body, config);
       setIsLoading(true);
       setData((prev) => [...prev, res.data]);
       setMessage("Success !!");
-      if(path) {
+      if (path) {
         navigate(path);
       }
     } catch (err) {
@@ -40,18 +50,16 @@ const UseApi = () => {
     }
   };
 
-  const patch = async (url, body, path, config) => {
+  const patch = async (body, path, config) => {
     try {
       const res = await axios.patch(url, body, config);
       setIsLoading(true);
-      //   ? Search this
       if (path) {
         navigate(path);
       }
       setData((prev) =>
         prev.map((item) => (item.id === body.id ? res.data : item))
       );
-      // setData(res.data)
       setMessage("Success !!");
     } catch (err) {
       setError(err);
@@ -60,9 +68,9 @@ const UseApi = () => {
     }
   };
 
-  const del = async (url, id, config) => {
+  const del = async (id, config) => {
     try {
-      await axios.delete(url, config); 
+      await axios.delete(`${url}/${id}`, config);
       setIsLoading(true);
       setData((prev) => prev.filter((item) => item.id !== id));
     } catch (err) {
@@ -77,10 +85,12 @@ const UseApi = () => {
     isLoading,
     error,
     message,
+    item,
     get,
     post,
     patch,
     del,
+    getById
   };
 };
 
